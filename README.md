8528953a02c7e8269b6572831b641b31205742ad
Sandrew94 <64556313+Sandrew94@users.noreply.github.com>
2021-05-01 21:48:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+<br />
+<p align="center">
+  <a href="https://www.codewars.com/users/Sandrew94">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center"># CodeWars__Challenges</h3>
+        <h5 align="center"> My username and current rank: </h5>
+        <p align="center"> 
+          <a href="https://www.codewars.com/users/Sandrew94">
+            <img src="images/rank.png" alt="Username-Rank" width="180" height="auto">
+          </a>
+        </p>
+
+<!-- TABLE OF CONTENTS -->
+<summary>
+<h2 style="display: inline-block">Table of Contents</h2></summary>
+<details open="open">  
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+- In this project i will often add a lot of challenges, that you can find on [CodeWars](https://www.codewars.com/dashboard), if you are stuck and you can't find a solution, just let's see mine and get a hint!
+- Or, i hope, you will do better than mine and you share your solutions!
+
+### Built With
+
+- [Javascript Vanilla](https://developer.mozilla.org/it/docs/Web/JavaScript)
+
+## Usage
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/Sandrew94/CodeWars___Challenges.git
+   ```
+2. Just check the exercise on the table at the end at the page.
+3. Find the row for the exercise you need to check.
+
+<!-- CONTRIBUTING -->
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+<!-- CONTACT -->
+
+## Contact
+
+Sandrew94 - [@My_twitter](https://twitter.com/AndreaSanti15) - [Email](santiandrea959@gmail.com)
+
+<!-- TABLE -->
+
+|          Challenges           |   Row   | Difficult (kyu) |
+| :---------------------------: | :-----: | :-------------: |
+|         Good vs Evil          |  1-113  |        6        |
+|    Stop gninnipS My sdroW!    | 119-136 |        6        |
+|       You're a square!        | 142-149 |        7        |
+| Find the next perfect square! | 154-167 |        7        |
---
+++ src/script.js
@@ -0,0 +1,171 @@
+//[EXERCISE] Good vs Evil
+//link: https://www.codewars.com/kata/52761ee4cffbc69732000738
+
+//My FIRST SOLUTION
+
+/* function goodVsEvil(good, evil) {
+  //String for good
+  const convertToArrNum = function (str) {
+    return str.split(" ").map(Number);
+  };
+
+  const goodArmy = convertToArrNum(good)
+    .map((el, i) => {
+      if (i === 0) return el * 1;
+      else if (i === 1) return el * 2;
+      else if (i === 2) return el * 3;
+      else if (i === 3) return el * 3;
+      else if (i === 4) return el * 4;
+      else return el * 10;
+    })
+    .reduce((acc, i) => acc + i, 0);
+
+  const evilArmy = convertToArrNum(evil)
+    .map((el, i) => {
+      if (i === 0) return el * 1;
+      else if (i === 1) return el * 2;
+      else if (i === 2) return el * 2;
+      else if (i === 3) return el * 2;
+      else if (i === 4) return el * 3;
+      else if (i === 5) return el * 5;
+      else return el * 10;
+    })
+    .reduce((acc, i) => acc + i, 0);
+
+  if (goodArmy > evilArmy) return "Battle Result: Good triumphs over Evil";
+  else if (goodArmy == evilArmy)
+    return "Battle Result: No victor on this battle field";
+  else return "Battle Result: Evil eradicates all trace of Good";
+} */
+
+/* //BEST PRACTICE ( NOT MINE)
+
+function goodVsEvil1(good, evil) {
+  var getWorth = function (side, worth) {
+    return side.split(" ").reduce(function (result, value, index) {
+      return result + worth[index] * value;
+    }, 0);
+  };
+
+  var result =
+    getWorth(good, [1, 2, 3, 3, 4, 10]) -
+    getWorth(evil, [1, 2, 2, 2, 3, 5, 10]);
+
+  return result > 0
+    ? "Battle Result: Good triumphs over Evil"
+    : result < 0
+    ? "Battle Result: Evil eradicates all trace of Good"
+    : "Battle Result: No victor on this battle field";
+}
+
+console.log(goodVsEvil("0 0 0 0 0 10", "0 1 1 1 1 0 0"));
+console.log(goodVsEvil1("0 0 0 0 0 10", "0 1 1 1 1 0 0")); */
+
+/* //Refactoring with OOP (JUST FOR FUN)
+class EpicBattle {
+  constructor(good, evil) {
+    this.good = good;
+    this.evil = evil;
+    console.log(this.finalResult(this.result(good, evil)));
+  }
+
+  convertToArrNum(str) {
+    return str.split(" ").map(Number);
+  }
+
+  mapElGood(el) {
+    return this.convertToArrNum(el)
+      .map((el, i) => {
+        if (i >= 0 && i <= 2) return el * (i + 1);
+        if (i === 3) return el * 3;
+        if (i === 4) return el * 4;
+        else return el * 10;
+      })
+      .reduce((acc, i) => acc + i, 0);
+  }
+
+  mapElEvil(el) {
+    return this.convertToArrNum(el)
+      .map((el, i) => {
+        if (i === 0) return el * 1;
+        if (i >= 1 && i <= 3) return el * 2;
+        if (i === 4) return el * 3;
+        if (i === 5) return el * 5;
+        else return el * 10;
+      })
+      .reduce((acc, i) => acc + i, 0);
+  }
+
+  result(good, evil) {
+    return this.mapElGood(good) - this.mapElGood(evil);
+  }
+
+  finalResult(result) {
+    return result > 0
+      ? "Battle Result: Good triumphs over Evil"
+      : result < 0
+      ? "Battle Result: Evil eradicates all trace of Good"
+      : "Battle Result: No victor on this battle field";
+  }
+}
+
+const epic = new EpicBattle("0 0 0 0 0 10", "0 1 1 1 1 0 0");
+console.log(epic); */
+
+////////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+
+//[EXERCISE] Stop gninnipS My sdroW!
+//link: https://www.codewars.com/kata/5264d2b162488dc400000001/javascript
+
+/* function spinWords(sentence) {
+  const sentenceArray = sentence.split(" ");
+  let result = "";
+  sentenceArray.map((str, i) => {
+    if (str.length >= 5) {
+      sentenceArray[i] = str.split("").reverse().join("");
+    } else {
+      sentenceArray[i] = str;
+    }
+    result = sentenceArray.join(" ");
+  });
+  return console.log(result);
+}
+
+spinWords("Just kidding there is still one more"); */
+
+///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+//[EXERCISE] You're a square!
+//link: https://www.codewars.com/kata/54c27a33fb7da0db0100040e
+
+/* const isSquare = function (n) {
+  const realSquare = Math.sqrt(n) % 1 === 0;
+  return realSquare;
+}; */
+
+///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+
+//[EXERCISE] Find the next perfect square!
+//link: https://www.codewars.com/kata/56269eb78ad2e4ced1000013
+
+/* 
+function findNextSquare(sq) {
+  const squareNumber = Math.sqrt(sq) % 1 === 0; // Check if it is a square number
+  if (squareNumber) {
+    let nextSquare = Math.sqrt(sq) + 1; //Take the sq number that was put in and add +1 because we wont to now the next one;
+    nextSquare *= nextSquare; //Then just multiply it by it's self to find the result of the next square;
+    return nextSquare;
+  } else {
+    return -1;
+  }
+} */
+
+///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
